9dc8b2eb25ae444bbbf8b8b415942f8db1f6d7bf
Scott Krohn <skrohn86@gmail.com>
2019-04-29 21:24:09 -0700

    Resolved more conflicts

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,7 @@
 var router = require('./router');
 
-<<<<<<< HEAD
 module.exports = function(app, passport) {
 	app.use('/api', router);
 	app.use('/server', router);
 	app.use('/auth', router);
 };
-=======
-
-module.exports = function (app) {
-	app.use('/api', apiRoutes);
-	app.use('/server', apiRoutes);
-};
->>>>>>> 57ecf903d27d19fd6d9e2e0873bc1f546237ccc1
