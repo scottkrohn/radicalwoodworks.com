4570925ea67d5f7c071fb0c79e4aa86f20302538
scottkrohn <skrohn86@gmail.com>
2019-05-24 23:22:52 -0700

    Added descriptioin header

--- client/components/edit-description/edit-description.js
+++ client/components/edit-description/edit-description.js
@@ -9,25 +9,23 @@ import ReactQuill from 'react-quill';
 import styles from 'client/components/edit-description/edit-description.less';
 
 class EditDescription extends PureComponent {
-    constructor(props) {
-        super(props);
-	}
-		
-		render = () => {
-			return (
-				<div className={styles.EditDescriptionContainer}>
-					<ReactQuill
-						value={this.props.description}
-						onChange={this.props.onChange}
-					/>
-				</div>
-			);
-		}
+  constructor(props) {
+    super(props);
+  }
+
+  render = () => {
+    return (
+      <div className={styles.EditDescriptionContainer}>
+        <div className={styles.SectionHeader}>Description</div>
+        <ReactQuill value={this.props.description} onChange={this.props.onChange} />
+      </div>
+    );
+  };
 }
 
 EditDescription.propTypes = {
-	description: PropTypes.string,
-	onChange: PropTypes.func,
+  description: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 export default EditDescription;
