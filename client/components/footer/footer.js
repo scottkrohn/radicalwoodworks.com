b3376cc1eedba9770411fe13ecdd0b308744bf0d
scottkrohn <skrohn86@gmail.com>
2019-05-29 17:00:12 -0700

    Added basic footer files

---
+++ client/components/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from 'client/components/footer/footer.less';
+
+const Footer = () => {
+  return (
+    <div className={styles.FooterContainer}>
+      <hr />
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
