d910d009fffdac2f51352e99f2a2f8018998bf68
scottkrohn <skrohn86@gmail.com>
2019-05-29 23:17:59 -0700

    finished footer

--- client/components/footer/footer.js
+++ client/components/footer/footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
+
+// Components
+import SocialIcons from 'client/components/social-icons/social-icons';
+// Styles
 import styles from 'client/components/footer/footer.less';
+
+const handleBackToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
 const Footer = () => {
   return (
     <div className={styles.FooterContainer}>
-      <hr />
-      Footer
+      <SocialIcons />
+      <div onClick={handleBackToTop} className={styles.BackToTop}></div>
+      <div className={styles.Copyright}>{new Date().getFullYear()} Radical Woodworks</div>
     </div>
   );
 };
