d0dfb35027fbc670451a8859c5b5c53ce2abaf51
scottkrohn <skrohn86@gmail.com>
2019-06-23 21:16:27 -0700

    Convert login-form to hooks

--- client/components/login-form/login-form.js
+++ client/components/login-form/login-form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { get } from 'lodash';
 
@@ -11,105 +11,97 @@ import EXCEPTIONS from 'constants/exceptions';
 // Styles
 import styles from 'client/components/login-form/login-form.less';
 
-class LoginForm extends Component {
-    constructor(props) {
-        super(props);
+const LoginForm = (props) => {
+  useEffect(() => {
+    const loginError = get(props, 'error');
+    if (loginError) {
+      renderLoginError();
     }
+  });
 
-    componentDidUpdate = (prevProps, prevState) => {
-        const loginError = get(this.props, 'error');
-        if (loginError) {
-            this.renderLoginError();
-        }
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        props.handleLogin(values.username, values.password);
+      }
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                this.props.handleLogin(values.username, values.password);
-            }
-        });
-    };
+  const renderLoginError = () => {
+    const errorMessage = EXCEPTIONS.getMessageForErrorCode(props.errorCode);
 
-    renderLoginError = () => {
-        const errorMessage = EXCEPTIONS.getMessageForErrorCode(this.props.errorCode);
+    notification.error({
+      message: 'Login Error',
+      description: errorMessage,
+    });
+  };
 
-        notification.error({
-            message: 'Login Error',
-            description: errorMessage,
-        });
-    };
+  const { getFieldDecorator } = props.form;
 
-    render = () => {
-        const { getFieldDecorator } = this.props.form;
+  const formItemLayout = {
+    labelCol: {
+      md: { span: 24 },
+      lg: { span: 3 },
+    },
+    wrapperCol: {
+      md: { span: 24 },
+      lg: { span: 16 },
+    },
+    layout: 'vertical',
+    labelAlign: 'left',
+  };
 
-        const formItemLayout = {
-            labelCol: {
-                md: { span: 24 },
-                lg: { span: 3 },
-            },
-            wrapperCol: {
-                md: { span: 24 },
-                lg: { span: 16 },
-            },
-            layout: 'vertical',
-            labelAlign: 'left',
-        };
+  const Label = (props) => {
+    return <span className={styles.FormLabel}>{props.label}</span>;
+  };
 
-        const Label = (props) => {
-            return <span className={styles.FormLabel}>{props.label}</span>;
-        };
+  const submitLayout = {
+    wrapperCol: {
+      md: { span: 24, offset: 0 },
+      lg: { span: 16, offset: 3 },
+    },
+  };
 
-        const submitLayout = {
-            wrapperCol: {
-                md: { span: 24, offset: 0 },
-                lg: { span: 16, offset: 3 },
-            },
-        };
-
-        return (
-            <div className={styles.LoginFormContainer}>
-                <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-                    <Form.Item label={<Label label="Username" />}>
-                        {getFieldDecorator('username', {
-                            rules: [
-                                {
-                                    required: true,
-                                    message: 'Username is required',
-                                },
-                            ],
-                        })(<Input size="large" />)}
-                    </Form.Item>
-                    <Form.Item label={<Label label="Password" />}>
-                        {getFieldDecorator('password', {
-                            rules: [
-                                {
-                                    required: true,
-                                    message: 'Password is required',
-                                },
-                            ],
-                        })(<Input size="large" type="password" />)}
-                    </Form.Item>
-                    <Form.Item {...submitLayout}>
-                        <Button
-                            id="submit-login-form"
-                            type="primary"
-                            htmlType="submit"
-                            className={styles.LoginSubmitButton}
-                        >
-                            Submit
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </div>
-        );
-    };
-}
+  return (
+    <div className={styles.LoginFormContainer}>
+      <Form {...formItemLayout} onSubmit={handleSubmit}>
+        <Form.Item label={<Label label="Username" />}>
+          {getFieldDecorator('username', {
+            rules: [
+              {
+                required: true,
+                message: 'Username is required',
+              },
+            ],
+          })(<Input size="large" />)}
+        </Form.Item>
+        <Form.Item label={<Label label="Password" />}>
+          {getFieldDecorator('password', {
+            rules: [
+              {
+                required: true,
+                message: 'Password is required',
+              },
+            ],
+          })(<Input size="large" type="password" />)}
+        </Form.Item>
+        <Form.Item {...submitLayout}>
+          <Button
+            id="submit-login-form" type="primary"
+            htmlType="submit" className={styles.LoginSubmitButton}
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
 LoginForm.propTypes = {
-    form: PropTypes.object,
-    handleLogin: PropTypes.func,
+  form: PropTypes.object,
+  handleLogin: PropTypes.func,
 };
 
 export default Form.create({ name: 'login_form' })(LoginForm);
